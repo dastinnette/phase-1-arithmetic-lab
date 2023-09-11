1a3861e60fca5e4c067a71ded876fafc3a2751ba
David Stinnette <dastinnette@gmail.com>
2023-09-11 14:21:17 -0400

    Fixed failing random integer test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const num1 = 31
 const num2 = 2
 const multiply = num1 * num2
 
-const random = Math.floor(Math.random() * 10)
+const random = Math.floor(Math.random() * 100)
 
 const num3 = 28
 const num4 = 6
